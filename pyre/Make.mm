bcbf69e20726ee31a0a2bd90450c763d33e615d2
Leif Strand <leif@geodynamics.org>
2005-09-20 23:53:18 +0000

    Utilize my new 'pyconfig' macro/tool to export Exchanger's config, and slurp Pythia's.  Added an __init__.py module. (Apparently the Python in this package isn't used???)  Changed contact e-mail to cig-mc.

--- pyre/Make.mm
+++ pyre/Make.mm
@@ -21,18 +21,23 @@ release: tidy
 update: clean
 	cvs update .
 
+init:
+	sed -e 's|@PACKAGE_VERSION@|1.0.0|g' < __init__.py.in > __init__.py
+
 #--------------------------------------------------------------------------
 #
 # export
 
 EXPORT_PYTHON_MODULES = \
+	__init__.py \
 	CoupledApp.py \
 	Coupler.py \
 	Exchanger.py \
 	Layout.py \
 
 
-export:: export-python-modules
+export:: init export-python-modules
+	$(RM) $(RMFLAGS) __init__.py __init__.pyc
 
 # version
 # $Id: Make.mm,v 1.2 2004/06/11 17:20:55 tan2 Exp $
