93bf84ef7f1b6d66548e38348372abe06dfd2ab2
Leif Strand <leif@geodynamics.org>
2006-09-26 00:39:50 +0000

    Fixed a glitch in 'setup.py':  install 'setuptools' itself.

--- setup.py
+++ setup.py
@@ -6,14 +6,22 @@
 from ez_setup import use_setuptools
 use_setuptools()
 
-from setuptools import setup
+import setuptools
 import sys
 
-setup(
+requirements = []
+
+if setuptools.bootstrap_install_from:
+    requirements.append(setuptools.bootstrap_install_from)
+    setuptools.bootstrap_install_from = None
+
+requirements.append('pythia >= 0.8-1.0dev-r4617, < 0.8-2.0a, == dev')
+
+setuptools.setup(
     script_args = (
     ['easy_install',
      '--find-links=svn://geodynamics.org/cig/cs/pythia/trunk#egg=pythia-dev'] +
     sys.argv[1:] +
-    ['pythia >= 0.8-1.0dev-r4617, < 0.8-2.0a, == dev']
+    requirements
     )
 )
